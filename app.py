b1c490fa78f6d39adbb4cbb488355c299582ea05
MaryamAbia <maryamabia14@gmail.com>
2025-07-10 16:27:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS utilisateurs (
 
 conn.commit()
 
-.stApp {
+st.App {
     background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-ai-image%2Fclear-nuclear-medicine-image-showing-distribution-radiotracer-within-body-highlighting-areas-high-uptake-set-specialized-imaging-center-with-advanced-technology_267592288.htm&psig=AOvVaw2-Q9eywj3K6ocud-cVbQpT&ust=1752247411104000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLi3zL3Mso4DFQAAAAAdAAAAABAE');
     background-size: cover;
     background-position: center;
