b30f89639a7deb47571600e32e199c500a4e6363
MaryamAbia <maryamabia14@gmail.com>
2025-07-10 17:50:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ st.markdown("""
     }
     .section-container {
         border-radius: 15px;
-        padding: 70px 40px;
+        padding: 90px 40px;
         margin-bottom: 30px;
         box-shadow: 0 8px 24px rgba(0,0,0,0.1);
         color: black;
