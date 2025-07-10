a7c5afa3df6b3506ff79931f303123f20f42f31c
MaryamAbia <maryamabia14@gmail.com>
2025-07-10 16:25:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,19 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS utilisateurs (
 
 conn.commit()
 
+.stApp {
+    background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-ai-image%2Fclear-nuclear-medicine-image-showing-distribution-radiotracer-within-body-highlighting-areas-high-uptake-set-specialized-imaging-center-with-advanced-technology_267592288.htm&psig=AOvVaw2-Q9eywj3K6ocud-cVbQpT&ust=1752247411104000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLi3zL3Mso4DFQAAAAAdAAAAABAE');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    color: black;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+
+
+
 # Fonction d'envoi email (modifie avec ton email / mdp)
 def envoyer_email(destinataire, sujet, message):
     sender_email = "maryamabia14@gmail.com"
